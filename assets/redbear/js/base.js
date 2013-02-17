e6cebf88b40e486534627b5048de1b9a69589c94
Chatri Sae-Tung <gearmag@gmail.com>
2013-02-18 01:46:59 +0700

    Edit layout.

--- assets/redbear/js/base.js
+++ assets/redbear/js/base.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var label = $('#small_label').html();
 
-    $('#page_title').mouseover(function(){
+    $('#page_title').mouseover(function () {
         $('#small_label').html("Grrr!");
     });
 
-    $('#page_title').mouseout(function(){
+    $('#page_title').mouseout(function () {
         $('#small_label').html(label);
+    });
+
+    var footer_label = $('#footer_small_label').html();
+
+    $('#footer_title').mouseover(function () {
+        $('#footer_small_label').html("Grrr!");
+    });
+
+    $('#footer_title').mouseout(function () {
+        $('#footer_small_label').html(footer_label);
     });
 });
