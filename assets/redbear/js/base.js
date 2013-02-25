7b0b808cf89349f9b832a63d29a64edb40b37e2c
Chatri Sae-Tung <gearmag@gmail.com>
2013-02-25 15:01:43 +0700

    Add first post, Change layout, Remove unused files.

--- assets/redbear/js/base.js
+++ assets/redbear/js/base.js
@@ -1,22 +1,3 @@
 $(document).ready(function () {
 
-    var label = $('#small_label').html();
-
-    $('#page_title').mouseover(function () {
-        $('#small_label').html("Grrr!");
-    });
-
-    $('#page_title').mouseout(function () {
-        $('#small_label').html(label);
-    });
-
-    var footer_label = $('#footer_small_label').html();
-
-    $('#footer_title').mouseover(function () {
-        $('#footer_small_label').html("Grrr!");
-    });
-
-    $('#footer_title').mouseout(function () {
-        $('#footer_small_label').html(footer_label);
-    });
 });
